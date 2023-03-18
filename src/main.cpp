aa07e8b926a68b66f17164f5be53380fce5f7327
Piotr Kopron <piotr.kopron_s@protonmail.com>
2023-03-18 04:04:27 +0100

    reqs + commmand handling stub

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,80 @@
 #include<iostream>
+#include<string>
+#include<sstream>
+#include<queue>
+#include<functional>
+#include<unordered_map>
+
+typedef std::queue<std::string> Command;
+typedef void (*CommandExecutor)(Command); 
+//ProductionLine active;
+
+
+//Also a nice way to visualize requirements for REPL interface
+const std::string helpmessage = 
+"Available commands: \n"
+"help : prints this message \n"
+"loadr <path> : loads production lines from path\n"
+"new <name> : creates new, empty production line\n"
+"add [-s] <b_name> <i_name> <pval> : adds a new production block b_name,\n"
+"then rescales it so that the total production of i_name in the entire line will be equal p_val\n"
+"if production block b_name already exists, does not add a 2nd time, only rescales.\n"
+"remove [-s] <b_name> : removes a production block \n"
+"rescale [-s] <i_name> <pval> : rescales the entire line so that the production of i_name reaches pval\n"
+"In add,remove and rescale commands, unless the -s option is specified, dump will be called immedietaly afterwards.\n"
+"save <path> : saves the current line to path \n"
+"dump [-a] : prints production values of current line to screen.\n"
+"if -a option is specified, all details of current production line will be printed."
+"quit : Exits the program."; //not given an executor, handled directly in main()
+
+Command parseCommand();
+void help(Command);
+void load(Command);
+void new_line(Command);
+void add(Command);
+void remove(Command);
+void rescale(Command);
+void save(Command);
+void dump(Command);
+const std::unordered_map<std::string,CommandExecutor> executors = initExecutors();
+const std::unordered_map<std::string,CommandExecutor> initExecutors()
+{
+    std::unordered_map<std::string,CommandExecutor> retval;
+    retval["help"] = help;
+    retval["load"] = load;
+    retval["new"] = new_line;
+    retval["add"] = add;
+    retval["remove"] = remove;
+    retval["rescale"] = rescale;
+    retval["save"] = save;
+    retval["dump"] = dump;
+}
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello World" << std:: endl;
+    //initialization
+    
+
+    std::cout << "Welcome to Helmod 2. \n" << std:: endl;
+    while (true)
+    {
+        std::queue<std::string> command = parseCommand();
+        std::string commandType = command.front();
+        if(commandType == "quit") break; 
+        if(executors.find(commandType) == executors.end())
+        {
+            std::cout << "invalid command: " << commandType << std::endl;
+            continue;
+        }
+        command.pop(); // a command executor does not need to hear its name, so we pop it.
+        executors.at(commandType)(command);
+        
+    }
+    
     return 0;
+}
+
+std::queue<std::string> parseCommand()
+{
+
 }
