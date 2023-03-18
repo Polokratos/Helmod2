a74505b607cd575e1b33521c44958ac6d1cac635
Piotr Kopron <piotr.kopron_s@protonmail.com>
2023-03-18 01:03:03 +0100

    spec clarification

--- src/Ingredient.hpp
+++ src/Ingredient.hpp
@@ -1,3 +1,3 @@
-//An ingredient is always uniquely defined by its name, therefore we can just typedef it as a string.
+//An ingredient is always uniquely defined by its name, and does not hold auxillary data, therefore we can just typedef it as a string.
 #include<string>
 typedef std::string Ingredient;
