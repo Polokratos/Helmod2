18212954f1bba107d8cc8c8a0c0345bf041de37c
Piotr Kopron <piotr.kopron_s@protonmail.com>
2023-03-18 01:03:26 +0100

    recipe remove stub

--- src/Recipe.hpp
+++ src/Recipe.hpp
@@ -2,8 +2,10 @@
 #define HELMOD_RECIPE_HPP
 #endif
 
-#include <unordered_map>
+#include <vector>
+#include <tuple>
 #include <iostream>
+#include <map>
 
 #include"nlohmann/json.hpp"
 using json = nlohmann::json;
@@ -12,26 +14,31 @@ using json = nlohmann::json;
 class Recipe
 {
 private:
+    std::string name; //name of recipe
+    static std::map<std::string,Recipe> all_recipes; //recipes cannot be duplicative. This static map ensures this.
     /**
      * Change in stock after 1 production cycle.
-     * if delta["xyz"] < 0, recipe used some xyz
-     * if delta["xyz"] > 0, recipe created some xyz
-     * if delta["xyz"] = 0, recipe does not care for xyz
+     * Using a map here is unnescesary, since this object will be always read in full anyway.
     */
-    std::unordered_map<Ingredient,int> delta;
+    std::vector<std::tuple<Ingredient,int>> delta;
     /**
      * Time required for 1 production cycle.
     */
     double process_time;
+
+    //Each recipe is a singleton, therefore we block copy constructor and '=' operator.
+    Recipe(const Recipe &tc){}
+    Recipe& operator=(const Recipe &tc){}
+
 public:
     //Explicit constructor
-    Recipe(const double time,const std::unordered_map<Ingredient,int>d):delta(d){}
+    Recipe(const std::string name, const double time,const std::vector<std::tuple<Ingredient,int>>d);
     //JSON object constructor
-    Recipe(json builder) 
-    {
-        delta = builder.at("delta").get<std::unordered_map<Ingredient,int>>();
-    }
+    Recipe(json builder);
 
-    
-    
+
+    static std::map<std::string, Recipe> getAllRecipes() {return all_recipes;}
+    std::vector<std::tuple<Ingredient,int>> getDelta() const {return this->delta;}
+    std::string getName() const {return name;}
+    double getTime() const {return process_time;}
 };
